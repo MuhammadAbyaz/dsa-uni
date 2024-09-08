16ac1cbda930362e1e62f26894d5df487c36f880
Muhammad Abyaz <m.abyaz681@gmail.com>
2024-09-08 13:01:58 +0500

    fix: task two vector class

--- taskThree.cpp
+++ taskThree.cpp
@@ -1,45 +1,86 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Vector
 {
 private:
-    int i;
-    int j;
-    int k;
+    vector<int> data;
+    int dimensions;
 
 public:
-    Vector(int first, int second, int third) : i(first), j(second), k(third)
+    Vector(int d) : dimensions(d)
     {
     }
-    void scalar_multiplication(int scalar)
+    void input_vector()
     {
-        i *= scalar;
-        j *= scalar;
-        k *= scalar;
+        for (int i = 0; i < dimensions; ++i)
+        {
+            cout << "Enter " << i + 1 << " value: ";
+            int input;
+            cin >> input;
+            data.push_back(input);
+        }
+    }
+    Vector scalar_multiplication(int scalar)
+    {
+        Vector res(dimensions);
+        for (int i = 0; i < dimensions; ++i)
+        {
+            res.data.push_back(data[i] * scalar);
+        }
+        return res;
     }
     Vector operator+(Vector &second)
     {
-        return Vector(i + second.i, j + second.j, k + second.k);
+        if (dimensions != second.dimensions)
+        {
+            throw invalid_argument("Vector addition is not posible");
+        }
+        Vector res(dimensions);
+        for (int i = 0; i < dimensions; ++i)
+        {
+            res.data.push_back(data[i] + second.data[i]);
+        }
+        return res;
     }
-    int operator-(Vector &second)
+    int operator*(Vector &second)
     {
-        return (i * second.i) + (j * second.j) + (k * second.k);
+        if (dimensions != second.dimensions)
+        {
+            throw invalid_argument("Vector multiplication is not posible");
+        }
+        int res = 0;
+        for (int i = 0; i < dimensions; ++i)
+        {
+            res += data[i] * second.data[i];
+        }
+        return res;
     }
     void print()
     {
-        cout << i << "i + " << j << "j + " << k << "k" << endl;
+        char unit_vector = 'i';
+        for (int i = 0; i < dimensions; ++i)
+        {
+            cout << data[i] << unit_vector << " ";
+            unit_vector++;
+        }
+        cout << endl;
     }
 };
 int main()
 {
-    Vector A(16, -6, 7);
-    Vector B(4, 2, -3);
+    Vector A(3);
+    Vector B(3);
+    A.input_vector();
+    B.input_vector();
     int x = 2;
     int y = -5;
-    A.scalar_multiplication(x);
-    B.scalar_multiplication(y);
-    Vector C = A + B;
-    C.print();
+    Vector C = A.scalar_multiplication(x);
+    Vector D = B.scalar_multiplication(y);
+    Vector F = C + D;
+    F.print();
+    int dot_product = A * B;
+    cout << dot_product;
     return 0;
 }
